0f5b4af4c55507292802bf531926bf4974bf595e
Guido Rodriguez <guido292001@gmail.com>
2021-05-29 08:07:16 -0300

    Restoring name of main file

---
+++ labiBot.js
@@ -0,0 +1,51 @@
+
+console.log("LABi Bot Docs dice hola");
+
+require("dotenv").config();
+
+const BOT = require("./respuestas");
+
+const DISCORD_API = require("discord.js");
+const DISCORD_CLIENT = new DISCORD_API.Client();
+
+DISCORD_CLIENT.login(process.env.TOKEN);
+
+DISCORD_CLIENT.on("ready", () => {
+
+    console.log("LABi Bot Docs listo para recibir ordenes");
+});
+
+DISCORD_CLIENT.on("message",catchMessage);
+
+function catchMessage(query){
+
+    const stringMensaje = query.content.toLowerCase();
+
+    if((query.channel.id === BOT.RESPUESTA.config.channel_id) && (stringMensaje.includes("!labi")) && (stringMensaje.charAt(0) ==='!')){
+
+        const COMMAND = stringMensaje.split(" ",2)[1].trim();
+
+        console.log(COMMAND);
+
+        query.reply(BOT.RESPUESTA[COMMAND]);
+    }
+
+    else if ((query.channel.id != BOT.RESPUESTA.channel_id) && !(query.author.bot) && (stringMensaje.includes("!labi")) && (stringMensaje.charAt(0)==='!')){
+
+        query.delete({ timeout: 10000 });
+
+        query.reply(BOT.RESPUESTA.wrong_channel)
+            
+            .then(respuestaError => {
+
+                respuestaError.delete({ timeout: 10000 });
+            }
+        );
+    }
+
+    else{
+
+        console.log("Mensaje ignorado");
+    }
+}
+
