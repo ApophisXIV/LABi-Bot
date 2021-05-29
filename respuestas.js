c758bd30a6deb1bef301cf3c904a87092360d4a9
Guido Rodriguez <guido292001@gmail.com>
2021-05-29 09:33:59 -0300

    Decoupling gifs URL from error messages

--- respuestas.js
+++ respuestas.js
@@ -16,10 +16,17 @@ const RESPUESTA = {
 
         channel_id: process.env.CHANNEL_ID,
     },
-    
-    wrong_channel: "¿Quién me despertó? :rage: No hay nadie venga más tarde. Atiendo solo en el canal designado :point_right_tone1: 𝙇𝘼𝘽𝙞-𝘽𝙤𝙩-📝\n\n https://raw.githubusercontent.com/ApophisXIV/LABi-Bot/master/resources/responseGifs/labiBotWrongChannel.gif",
 
-    unknown_command: "El comando es incorrecto. Volvé a intentar",
+    gifs:{
+
+        wrong_channel: "https://raw.githubusercontent.com/ApophisXIV/LABi-Bot/master/resources/responseGifs/labiBotWrongChannel.gif",
+        
+        unknown_command: "https://raw.githubusercontent.com/ApophisXIV/LABi-Bot/master/resources/responseGifs/unknowCommand.gif"
+    },
+    
+    wrong_channel: "¿Quién me despertó? :rage: No hay nadie venga más tarde. Atiendo solo en el canal designado :point_right_tone1: 𝙇𝘼𝘽𝙞-𝘽𝙤𝙩-📝",
+
+    unknown_command: "El comando es incorrecto o todavia no se inventó. Volvé a intentar.",
 
     ping: "Pong 🏓",
 
